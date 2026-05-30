d85b84e4644e086db41b80db1008ae064a236855
newzelandpanda <newzelandpanda@gmail.com>
2026-05-31 01:31:04 +0200

    fix FBCard declaration

--- src/App.jsx
+++ src/App.jsx
@@ -1046,8 +1046,9 @@ function ResumePage() {
     </div>
   );
 }
+
+function FBCard({ icon, title, text, color }) {
   return (
-    <div style={{ background: "white", border: `1.5px solid ${TM}`, borderLeft: `4px solid ${color}`, borderRadius: 20, padding: 22 }}>
       <div style={{ fontWeight: 700, fontSize: 15, color: DARK, marginBottom: 10 }}>{icon} {title}</div>
       <div style={{ fontSize: 14, lineHeight: 1.75, color: GREY, whiteSpace: "pre-line" }}>{text}</div>
     </div>
